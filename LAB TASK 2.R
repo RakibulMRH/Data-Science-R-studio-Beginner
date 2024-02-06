d7d18fa77d57958e8dbeacec3c343f2065a3a992
MD. RAKIBUL HASAN <rakibulmrh4bd@gmail.com>
2024-02-06 15:22:48 +0600

    null

--- LAB TASK 2.R
+++ LAB TASK 2.R
@@ -26,9 +26,47 @@ for (x in 1:2){
   }
 }
 
-a <- c(1,2,5,3,6,-2,4)
+#vector element index starts from 1
+a <- c(1,2,5,3,6,-2,4) 
 b <- c("one","two","three")
 C <- c(TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
-a
-b
-c
+cat(a,b,c)
+cat(a[1:4])
+cat(a[c(1,4)])
+Name <- c("hello","world")
+nchar(Name)
+Name <- "Hello World"
+nchar(Name)
+
+X <- c(12,4,5)
+X+2
+X <- sort(X) #ascending order by default
+
+Y <- c("B", "C", "D")
+sort(Y,decreasing = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
