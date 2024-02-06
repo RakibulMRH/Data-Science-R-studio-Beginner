fa305313c65a556a159162aac10c8004ab9517d9
MD. RAKIBUL HASAN <rakibulmrh4bd@gmail.com>
2024-02-06 16:41:48 +0600

    lab2

--- LAB TASK 2.R
+++ LAB TASK 2.R
@@ -44,12 +44,24 @@ X <- sort(X) #ascending order by default
 
 Y <- c("B", "C", "D")
 sort(Y,decreasing = TRUE)
+mymatrix <- matrix(1:20,nrow = 5, ncol = 4, TRUE)
+mymatrix
 
+cells <- c(1:4)
+rnames <- c("R1","R2")
+cnames <- c("C1", "C2")
+mymatrix <- matrix(cells, nrow =2, ncol =2, byrow = FALSE, dimnames = list(rnames,cnames))
+print(mymatrix[1,1])
+cat(mymatrix[1,1],mymatrix[2,2], sep =" " )
+cat(mymatrix[c(1,1),c(2,2)], sep =" " )
 
+mymatrix2 <- matrix (1:20, nrow = 5, ncol = 4)
+cat(mymatrix2[1,c(2,3)], sep =" " )
 
+#Array
 
-
-
+myarray <- array(1:24,c(2,3,2))
+myarray[1,2,1]
 
 
 
